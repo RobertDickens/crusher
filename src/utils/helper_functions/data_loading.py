a76adea6de1c62a3eacb8de9b091f8269d860721
RobertDickens <robertdickens90@gmail.com>
2020-09-16 23:28:33 +0100

    various

--- src/utils/helper_functions/data_loading.py
+++ src/utils/helper_functions/data_loading.py
@@ -3,9 +3,8 @@ from orm.orm import ExchangeOddsSeriesItem, Runner
 from utils.helper_functions import preprocessing as pr
 
 
-# Modelling P/L from under 2.5 goals
 def get_odds_data(runner, divisions, in_play, item_freq_type_code,
-                  preprocess=True):
+                  convert_to_game_time=False):
     with dbm.get_managed_session() as session:
         runner = Runner.get_by_code(session, runner)
         df = ExchangeOddsSeriesItem.get_series_items_df(session,
@@ -13,11 +12,9 @@ def get_odds_data(runner, divisions, in_play, item_freq_type_code,
                                                         runner_uid=runner.runner_uid,
                                                         in_play=in_play,
                                                         item_freq_type_code=item_freq_type_code)
-        if preprocess:
-            df = df.sort_values(['series_uid', 'published_datetime'], ascending=True)
+        df = df.sort_values(['series_uid', 'published_datetime'], ascending=True).reset_index(drop=True)
+        if convert_to_game_time:
             df = pr.convert_published_datetime_to_game_time(df)
             df = df[['series_uid', 'game_time', 'ltp']]
-            return df
-        else:
-            return df
-
+        df = df[['series_uid', 'published_datetime', 'ltp']]
+        return df
