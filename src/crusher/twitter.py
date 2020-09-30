c9701da60560d9782af773975df1f85b7be0c48a
RobertDickens <robertdickens90@gmail.com>
2020-09-30 16:28:02 +0100

    added twitter classes for all premier league teams

--- src/crusher/twitter.py
+++ src/crusher/twitter.py
@@ -26,3 +26,73 @@ class ChelseaFC:
 class TottenhamFC:
     twitter_id = '121402638'
     handle = '@SpursOfficial'
+
+
+class AstonVillaFC:
+    twitter_id = '27489253'
+    handle = '@AVFCOfficial'
+
+
+class BrightonFC:
+    twitter_id = '57606154'
+    handle = '@OfficialBHAFC'
+
+
+class BurnleyFC:
+    twitter_id = '441309926'
+    handle = '@BurnleyOfficial'
+
+
+class EvertonFC:
+    twitter_id = '15891449'
+    handle = '@Everton'
+
+
+class FulhamFC:
+    twitter_id = '22903812'
+    handle = '@FulhamFC'
+
+
+class LeedsUtd:
+    twitter_id = '1024095475'
+    handle = '@LUFC'
+
+
+class LeicesterFC:
+    twitter_id = '122392884'
+    handle = '@LCFC'
+
+
+class ManchesterCity:
+    twitter_id = '14573900'
+    handle = '@ManCity'
+
+
+class ManchesterUtd:
+    twitter_id = '558797310'
+    handle = '@ManUtd'
+
+
+class SheffieldUtd:
+    twitter_id = '63463538'
+    handle = '@SheffieldUnited'
+
+
+class SouthamptonFC:
+    twitter_id = '46143892'
+    handle = '@SouthamptonFC'
+
+
+class WestBromichAlbion:
+    twitter_id = '280412626'
+    handle = '@WBA'
+
+
+class WestHamUtd:
+    twitter_id = '156247074'
+    handle = '@WestHam'
+
+
+class WolverhamptonWanderers:
+    twitter_id = '49296276'
+    handle = '@Wolves'
