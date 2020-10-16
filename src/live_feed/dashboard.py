84b51670b3cc58ae94ecab0eea3086ad5940e0ca
RobertDickens <robertdickens90@gmail.com>
2020-10-16 22:26:29 +0100

    improved bookie odds stream

---
+++ src/live_feed/dashboard.py
@@ -0,0 +1,54 @@
+from multiprocessing import Process
+from crusher import twitter as tw
+from live_feed.twitter.twitter_listener import listen_for_teams
+import json
+import requests
+from datetime import date
+
+from live_feed.the_odds_api.bookie_odds_collector import BookieOddsCollector
+from live_feed.the_odds_api.bookie_odds_collector import the_odds_api_league_code
+from crusher.division import DivisionCodeEnum as DCEnum
+
+
+def twitter_listener():
+    listen_for_teams([tw.ManchesterCity,
+                      tw.ArsenalFC,
+                      tw.EvertonFC,
+                      tw.LiverpoolFC,
+                      tw.NewcastleUtd,
+                      tw.ManchesterUtd,
+                      tw.BournemouthAFC,
+                      tw.QPR])
+
+
+def bookie_odds_streamer():
+    api_key = 'C:\\Users\\rober\\crusher\\bookie_odds\\api_key.json'
+    with open(api_key, 'r') as f:
+        api = json.load(f)
+
+    # --------Config-------- #
+    API_KEY = api['api_key']
+    SPORT = the_odds_api_league_code[DCEnum.PREMIER_LEAGUE]
+    REGION = 'uk'
+    MARKET = 'h2h'
+
+    # Run
+    bookie_odds_collector = BookieOddsCollector(api_key=API_KEY,
+                                                sport=SPORT,
+                                                region=REGION,
+                                                mkt=MARKET)
+
+    save_path = 'C:\\Users\\rober\\sport_data\\bookie_odds_2020_10_16.csv'
+    bookie_odds_collector.stream_live_odds(log_odds=False,
+                                           save_path=save_path,
+                                           match_date_stream_filter=date(2020, 10, 17),
+                                           refresh_rate_seconds=300)
+
+
+if __name__ == '__main__':
+  p1 = Process(target=twitter_listener)
+  p1.start()
+  p2 = Process(target=bookie_odds_streamer)
+  p2.start()
+  p1.join()
+  p2.join()
--- src/playground/playground.py
+++ src/playground/playground.py
@@ -1,4 +1,21 @@
-from crusher import twitter as tw
-from live_feed.twitter.twitter_listener import listen_for_teams
+from multiprocessing import Process
 
-listen_for_teams([tw.EvertonFC, tw.BrightonFC, tw.QPR, tw.SheffieldWednesday])
+
+def func1():
+    print('func1: starting')
+    for i in range(10000000):
+        print(i)
+
+
+def func2():
+    print('func2: starting')
+    for i in range(10000000):
+        print('a')
+
+if __name__ == '__main__':
+  p1 = Process(target=func1)
+  p1.start()
+  p2 = Process(target=func2)
+  p2.start()
+  p1.join()
+  p2.join()
