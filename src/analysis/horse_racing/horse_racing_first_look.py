bb810a41b0db3c834740a04d839d11549993fbb8
RobertDickens <robertdickens90@gmail.com>
2020-10-31 11:23:15 +0000

    more work

--- src/analysis/horse_racing/horse_racing_first_look.py
+++ src/analysis/horse_racing/horse_racing_first_look.py
@@ -9,33 +9,98 @@ from crusher.sport import SportCodeEnum as SCEnum
 from crusher.runner import RunnerCodeEnum as RCEnum
 from crusher.market_type import MarketTypeCodeEnum as MTCEnum
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import GroupKFold, StratifiedKFold, StratifiedShuffleSplit, train_test_split
+from sklearn.metrics import cohen_kappa_score, accuracy_score, balanced_accuracy_score
+
+
 # Get win markets where preoff volume > 100k
+def classify_odds_movements(odds_pct_change):
+    if odds_pct_change < -0.005:
+        return 'short'
+    if odds_pct_change > 0.005:
+        return 'drift'
+    else:
+        return 'oscillate'
+
+
+def refactor_data(df, off_time, resample_freq=None, n_lags=5):
+    df = df.loc[df['published_datetime'] > off_time - relativedelta(minutes=180)]
+    if df.empty:
+        return df
+    df = df.drop(['series_uid', 'series_item_uid', 'runner_uid', 'in_play'], axis=1)
+    df = df.sort_values('published_datetime', ascending=True)
+    df = df.set_index('published_datetime')
+    df['min_to_off'] = df.index.map(lambda x: abs((x - off_time).total_seconds() / 60.0))
+    if resample_freq:
+        df = df.resample(resample_freq).mean()
+    df['volume_change'] = df['traded_volume'].diff()
+    df['odds_pct_change'] = df['ltp'].pct_change()
+    df['volume_pct_change'] = df['traded_volume'].pct_change()
+    df['rolling_max'] = df['ltp'].expanding().max()
+    df['rolling_min'] = df['ltp'].expanding().min()
+    df['rolling_previous_max'] = df['rolling_max'].shift()
+    df['rolling_previous_min'] = df['rolling_min'].shift()
+    df['previous_ltp'] = df['ltp'].shift()
+    df['odds_movement'] = df['odds_pct_change'].apply(lambda x: classify_odds_movements(x))
+    df['series_uid'] = series_uid
+
+    for n in range(1, n_lags):
+        df[f'lag_ltp_{n}'] = df['ltp'].shift(n)
+        df[f'volume_change_lag_{n}'] = df['volume_change'].shift(n)
+        df[f'odds_change_lag_{n}'] = df['odds_pct_change'].shift(n)
+        df[f'volume_pct_change_lagged_{n}'] = df['volume_pct_change'].shift(n)
+    df = df.dropna()
+
+    # fig, ax = plt.subplots()
+    # colors = {'drift': 'green', 'short': 'blue', 'oscillate': 'grey'}
+    # ax.scatter(df.index, df['ltp'], c=df['odds_movement'].apply(lambda x: colors[x]))
+    # plt.show()
+
+    return df
+
+
 with dbm.get_managed_session() as session:
     raw_df = ExchangeOddsSeriesItem.get_series_items_df(session, market_type_code=MTCEnum.WIN,
-                                                        in_play=False, min_market_pre_off_volume=1000000,
+                                                        in_play=False, min_market_pre_off_volume=100000,
                                                         runner_codes=[RCEnum.FAVOURITE])
     # Create features
-    df = pd.DataFrame(columns=['volume_delta', 'ltp', 'ltp_delta', 'min_to_off'])
+    df = pd.DataFrame()
     for series_uid in raw_df['series_uid'].unique():
+        sub_df = raw_df[raw_df['series_uid'] == series_uid]
         off_time = ExchangeOddsSeries.get_by_uid(session, int(series_uid)).market.off_time
-        sub_df = raw_df.loc[raw_df['series_uid'] == series_uid]
-        sub_df = sub_df.loc[sub_df['published_datetime'] > off_time - relativedelta(minutes=30)]
-        sub_df = sub_df.sort_values('published_datetime', ascending=True)
-        sub_df['volume_delta'] = sub_df['traded_volume'].diff()
-        sub_df['ltp_delta'] = sub_df['ltp'].diff()
-        sub_df = sub_df.dropna()
-        sub_df['min_to_off'] = sub_df['published_datetime'].apply(lambda x: (x - off_time).total_seconds() / 60.0)
-        sub_df = sub_df.set_index(['published_datetime'])
-        # create figure and axis objects with subplots()
-        fig, ax = plt.subplots()
-        ax.plot(sub_df.index, sub_df.ltp, color="red", marker="o")
-        ax.set_xlabel("time", fontsize=14)
-        ax.set_ylabel("ltp", color="red", fontsize=14)
-        ax2 = ax.twinx()
-        ax2.plot(sub_df.index, sub_df["volume_delta"], color="blue", marker="o")
-        ax2.set_ylabel("traded_volume", color="blue", fontsize=14)
-        plt.show()
+        sub_df = refactor_data(sub_df, off_time, '10s')
         df = df.append(sub_df)
     del raw_df
 
-    print(df)
+    df = df.drop(['ltp', 'traded_volume', 'odds_pct_change', 'volume_change', 'rolling_max', 'rolling_min',
+                  'volume_pct_change'], axis=1)
+    df = df[df['min_to_off'] < 5]
+    groups = df['series_uid'].values
+    X = df.drop(['odds_movement'], axis=1)
+    #X = X[['previous_ltp', 'rolling_previous_max', 'odds_change_lag_1', 'volume_pct_change_lagged_1']]
+    y = df[['odds_movement']]
+    kf = StratifiedKFold(n_splits=3)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=groups)
+    for series_uid in X_train['series_uid'].unique():
+        if series_uid in X_test['series_uid'].unique():
+            raise ValueError("That's a bad a split!")
+
+
+    # for train_index, test_index in kf.split(X, y, groups=groups):
+    #    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+    #    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+    #    for series_uid in X_train['series_uid'].unique():
+    #        if series_uid in X_test['series_uid'].unique():
+    #            raise ValueError("That's a bad a split!")
+    #    dt = GradientBoostingClassifier()
+    #    dt.fit(X_train, y_train)
+    #    predictions = dt.predict(X_test)
+    #    feature_importances = dict(zip(X_train.columns, dt.feature_importances_))
+    #    kappa_score = cohen_kappa_score(predictions, y_test)
+    #    accuracy_score_ = accuracy_score(predictions, y_test)
+    #    balanced_accuracy_score_ = balanced_accuracy_score(predictions, y_test)
+    #    print(kappa_score)
+    #    print(accuracy_score_)
+    #    print(balanced_accuracy_score_)
